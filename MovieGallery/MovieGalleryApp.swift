32059577d134a3683632b68a1925896112bceed7
Carlos Augusto dos Santos Filho <carlosfilho.canal@icloud.com>
2024-01-02 04:53:54 +0000

    Initial Commit

---
+++ MovieGallery/MovieGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieGalleryApp.swift
+//  MovieGallery
+//
+//  Created by Carlos dos Santos Filho on 02/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MovieGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
